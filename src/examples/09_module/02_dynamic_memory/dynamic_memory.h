4fe5268163f744d4541b94ad6495d0ed0539d0da
Art Gonzalez <arturo.gonzalez@austincc.edu>
2022-04-28 03:30:02 +0000

    use dynamic arrays

--- src/examples/09_module/02_dynamic_memory/dynamic_memory.h
+++ src/examples/09_module/02_dynamic_memory/dynamic_memory.h
@@ -1,2 +1,10 @@
 //Create allocate and release function
+#include<iostream>
+#include<memory>
 
+void use_dynamic_memory(const int num);
+int* get_dynamic_memory(const int num);
+void limit_dynamic_memory_scope(const int num);
+int* get_dynamic_array(const int num);
+void delete_dynamic_array(int* array);
+void use_dynamic_array(const int num);
--- src/examples/09_module/02_dynamic_memory/main.cpp
+++ src/examples/09_module/02_dynamic_memory/main.cpp
@@ -1,5 +1,21 @@
+#include "dynamic_memory.h"
+#include<memory>
+
+using std::cout; using std::unique_ptr; using std::make_unique;
+
 int main() 
 {
-	
+	/*int* num = new int(5);//create dynamic memory
+	cout<<"\nRaw/Naked Pointer value: "<<*num<<"\n";//use memory
+	delete num;//delete memory
+	num = nullptr;
+
+	unique_ptr<int> num1 = make_unique<int>(10);
+	cout<<"\nNum1 value: "<<*num1<<"\n";*/
+	//use_dynamic_memory(3);
+	//limit_dynamic_memory_scope(3);
+	use_dynamic_array(3);
+	cout<<"Exited function\n";
+
 	return 0;
 }
