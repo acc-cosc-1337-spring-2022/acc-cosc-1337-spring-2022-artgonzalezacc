131f8412f1e7ffdd2e9a72b4397f922373b3ac2f
Art Gonzalez <arturo.gonzalez@austincc.edu>
2022-04-28 03:26:43 +0000

    stack arrays

--- src/examples/10_module/01_arrays_mem/arrays_mem.h
+++ src/examples/10_module/01_arrays_mem/arrays_mem.h
@@ -1,3 +1,14 @@
 //arrays_mem.h - stack array example
+#include<iomanip>
+#include<iostream>
+#include<string>
+const int ROWS = 10;
+const int COLS = 10;
 
 //function prototype for stack_array
+void use_stack_array();
+void array_months();
+void arrays_and_pointers();
+void display_array(int* nums, const int SIZE);
+void populate_times_table(int times_table[][COLS], const int ROWS);
+void display_times_table(int times_table[][COLS], const int ROWS);
