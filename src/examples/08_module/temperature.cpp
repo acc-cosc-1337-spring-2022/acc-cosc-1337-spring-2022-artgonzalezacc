106d01765021832df6ffe9f22f78e4b886b5e4dd
Art Gonzalez <arturo.gonzalez@austincc.edu>
2022-04-28 03:23:53 +0000

    write/read classes to file

--- src/examples/08_module/temperature.cpp
+++ src/examples/08_module/temperature.cpp
@@ -1,1 +1,43 @@
 //function code
+#include "temperature.h"
+
+using std::cout; using std::ofstream; using std::ifstream;
+
+void write_to_file()
+{
+    ofstream out_file;
+
+    cout<<"Open file\n";
+    out_file.open("names.dat");
+
+    cout<<"Writing to file...\n";
+    out_file<<"C++\n";
+    out_file<<"Python\n";
+    out_file<<"Java\n";
+
+    cout<<"Close file\n";
+    out_file.close();
+}
+
+void read_from_file()
+{
+    ifstream in_file;
+    cout<<"Open file\n";
+    in_file.open("names.dat");
+
+    cout<<"Read from file\n";
+
+    std::string name;
+
+    if(in_file.is_open())
+    {
+        while(in_file>>name)
+        {
+            cout<<name<<"\n";
+        }
+    }
+
+    cout<<"Close the file\n";
+    in_file.close();
+
+}
--- src/examples/08_module/temperature_data.cpp
+++ src/examples/08_module/temperature_data.cpp
@@ -1,1 +1,42 @@
 //temperature_data.cpp
+#include "temperature_data.h"
+
+using std::ofstream; using std::ifstream;
+
+void TemperatureData::save_temps(const std::vector<Temperature>& temps)
+{
+    ofstream out_file;
+    out_file.open(file_name);
+
+    for(auto temp: temps)
+    {
+        out_file<<temp.get_hour();
+        out_file<<" ";
+        out_file<<temp.get_reading();
+        out_file<<"\n";
+    }
+
+    out_file.close();
+}
+
+std::vector<Temperature> TemperatureData::get_temps()
+{
+    std::vector<Temperature> temps;
+    ifstream in_file;
+    in_file.open(file_name);
+
+    int hour;
+    double reading;
+
+    if(in_file.is_open())
+    {
+        while(in_file>>hour>>reading)
+        {
+            Temperature temp(hour, reading);//create an instance/object
+            temps.push_back(temp);
+        }
+    }
+
+    in_file.close();
+    return temps;
+}
