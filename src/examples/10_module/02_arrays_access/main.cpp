6ff59387df1312b1b76678849c106073a1743892
Art Gonzalez <artgonzalezacc@users.noreply.github.com>
2022-11-01 18:25:24 -0500

    array list

--- src/examples/10_module/02_arrays_access/main.cpp
+++ src/examples/10_module/02_arrays_access/main.cpp
@@ -1,3 +1,20 @@
+#include "arrays_access.h"
+#include<iostream>
+
 int main()
-{
+{ 
+    int numbers[5];
+    ArrayList list(numbers);
+    list.Add(5);
+    list.Add(2);
+    list.Add(10);
+    list.Add(11);
+    list.Delete(5);
+    int* array = list.Get_Elements();
+    std::cout<<list.Size()<<"\n";
+
+    for(auto i=0; i < list.Size(); i++)
+    {
+        std::cout<<array[i]<<" \n";
+    }
 }
