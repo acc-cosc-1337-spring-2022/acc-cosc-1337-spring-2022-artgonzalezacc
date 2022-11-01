59ffee3ce615b2e9efb92a1f6d4eff7e4d460368
Art Gonzalez <artgonzalezacc@users.noreply.github.com>
2022-11-01 18:17:00 -0500

    array list class

--- src/examples/10_module/02_arrays_access/arrays_access.h
+++ src/examples/10_module/02_arrays_access/arrays_access.h
@@ -1,1 +1,16 @@
 //header
+class ArrayList
+{
+public:
+    ArrayList(int* array) : elements(array){}
+    void Add(int value);
+    void Delete(int value);
+    int Get_Index_Of(int value);
+    int Size()const{return current_index;}
+    int* Get_Elements(){return elements;}
+private:
+    int size;
+    int current_index{0};
+    int* elements;
+    void Adjust_Elements(int index);
+};
